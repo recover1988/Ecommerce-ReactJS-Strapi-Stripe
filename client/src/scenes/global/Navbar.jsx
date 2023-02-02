830f15586cb183c5d0b185aa889e3ea9ba8656d2
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-02 00:10:42 -0300

    add initialState

--- client/src/scenes/global/Navbar.jsx
+++ client/src/scenes/global/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Badge, Box, IconButton } from "@mui/material";
-import PersonOutline from "@mui/icons-material/PersonOutline";
+import PersonOutlined from "@mui/icons-material/PersonOutlined";
 import ShoppingBagOutlined from "@mui/icons-material/ShoppingBagOutlined";
 import MenuOutlined from "@mui/icons-material/MenuOutlined";
 import SearchOutlined from "@mui/icons-material/SearchOutlined";
@@ -11,8 +11,55 @@ import { shades } from "../../theme";
 const Navbar = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  return;
-  <Box></Box>;
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      width="100%"
+      height="60px"
+      backgroundColor="rgba(255, 255, 255,0.95)"
+      color="black"
+      position="fixed"
+      top="0"
+      left="0"
+      zIndex="1"
+    >
+      <Box
+        width="80%"
+        margin="auto"
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Box
+          onClick={() => navigate("/")}
+          sx={{ "&:hover": { cursor: "pointer" } }}
+          color={shades.secondary[500]}
+        >
+          Zhara
+        </Box>
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          columnGap="20px"
+          zIndex="2"
+        >
+          <IconButton sx={{ color: "black" }}>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton sx={{ color: "black" }}>
+            <PersonOutlined />
+          </IconButton>
+          <IconButton sx={{ color: "black" }}>
+            <ShoppingBagOutlined />
+          </IconButton>
+          <IconButton sx={{ color: "black" }}>
+            <MenuOutlined />
+          </IconButton>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Navbar;
