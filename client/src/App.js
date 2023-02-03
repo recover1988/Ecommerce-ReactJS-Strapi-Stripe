126e69d6a866186d41d983a9087514d040430130
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-02 21:43:27 -0300

    add footer

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Checkout from "./scenes/checkout/Checkout";
 import Confirmation from "./scenes/checkout/Confirmation";
 import Navbar from "./scenes/global/Navbar";
 import CartMenu from "./scenes/global/CartMenu";
+import Footer from "./scenes/global/Footer";
 
 // Esta funcion nos permite comenzar desde el top de la pagina cada vez que se encuentre una nueva
 const ScrollToTop = () => {
@@ -31,6 +32,7 @@ function App() {
           <Route path="checkout/success" element={<Confirmation />} />
         </Routes>
         <CartMenu />
+        <Footer />
       </BrowserRouter>
     </div>
   );
