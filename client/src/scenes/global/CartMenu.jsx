e8a71a9ed7c16420e61290fffc56cd193fcde621
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-03 18:02:53 -0300

    change localhost url for var .env

--- client/src/scenes/global/CartMenu.jsx
+++ client/src/scenes/global/CartMenu.jsx
@@ -69,7 +69,7 @@ const CartMenu = () => {
                       alt={item?.name}
                       width="123px"
                       height="164px"
-                      src={`http://localhost:1337${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
+                      src={process.env.API_URL_STRAPI + `${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
                     />
                   </Box>
                   <Box flex="1 1 60%">
