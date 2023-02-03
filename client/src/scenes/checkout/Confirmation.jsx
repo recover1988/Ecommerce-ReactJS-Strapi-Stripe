41a5efa2c3d492751b92a9abedd34f23ba4caf4e
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-03 18:17:57 -0300

    add confirmation page

--- client/src/scenes/checkout/Confirmation.jsx
+++ client/src/scenes/checkout/Confirmation.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Box } from "@mui/material";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
 
 const Confirmation = () => {
   return (
-    <div>Confirmation</div>
-  )
-}
+    <Box m="90px auto" width="80%" height="50vh">
+      <Alert severity="success">
+        <AlertTitle>Aprobado</AlertTitle>
+        La Orden fue registrada correctamente —{" "}
+        <strong>Gracias por comprar en Zarha!!!</strong>
+      </Alert>
+    </Box>
+  );
+};
 
-export default Confirmation
+export default Confirmation;
